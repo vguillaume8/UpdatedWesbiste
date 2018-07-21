f62fe52f8f0af7e280677781c23c1e65d0d84c20
Vinston Guillaume <vinstonguillaume@gmail.com>
2018-07-21 19:14:31 -0400

    many updates

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  var defaults = {
+      containerID: 'toTop', // fading element id
+    containerHoverID: 'toTopHover', // fading element hover id
+    scrollSpeed: 1200,
+    easingType: 'linear'
+  };
+
+
+  $().UItoTop({ easingType: 'easeOutQuart' });
+
+});
+</script>
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+$(".scroll").click(function(event){
+  event.preventDefault();
+  $('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+});
+});
