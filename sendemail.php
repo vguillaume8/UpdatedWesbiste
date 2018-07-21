c14da184e1a7b4ea4b43c53b10903f3e94b1c682
Vinston Guillaume <vinstonguillaume@gmail.com>
2018-07-21 19:30:47 -0400

    edited php

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to             = 'vinstonguillaume@gmail.com'; //put your email here
+    $to             = 'mailto:vinstonguillaume@gmail.com'; //put your email here
     $email_template = 'simple.html';  // will find it on email-templates/ directory
 
     $subject    = "SUBJECT";
